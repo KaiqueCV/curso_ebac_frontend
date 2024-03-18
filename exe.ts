848691679216566bbdb18a66dfdb69878be496bd
KaiqueCV <kaaique.13@hotmail.com>
2024-03-18 14:41:38 -0300

    entrega exe

---
+++ exe.ts
@@ -0,0 +1,9 @@
+// Esta função recebe dois números como argumentos e retorna seu produto
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+  // Esta função recebe uma string como argumento e retorna a string "Olá" concatenada com a string de entrada
+function greet(name: string): string {
+    return `Olá ${name}`;
+}
