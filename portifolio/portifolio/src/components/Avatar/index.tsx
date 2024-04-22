0d173a0d9dce05376cfaa5a8467a0e250102f2c0
KaiqueCV <kaaique.13@hotmail.com>
2024-04-22 06:36:45 -0300

    .

--- portifolio/portifolio/src/components/Avatar/index.tsx
+++
@@ -1,5 +0,0 @@
-import { FotoPerfil } from './styles'
-
-const Avatar = () => <FotoPerfil src="https://github.com/ogiansouza.png" />
-
-export default Avatar
--- portifolio/portifolio/src/components/Avatar/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const FotoPerfil = styled.img`
-  width: 128px;
-  height: 128px;
-  border-radius: 50%;
-  margin-bottom: 24px;
-`
